33e3dea6f56d89723998235535a99324fa2e9c23
skitarrata <skitarrata96@gmail.com>
2021-06-17 01:55:48 +0200

    subvetok

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int		ft_subvet(int arr[], int n, int *max)
 		printf("%d\n", vet[i]);
 } */
 
-void		ft_subsequence(int arr[], int n, int max)
+/* void		ft_subsequence(int arr[], int n, int max)
 {
 	int i;
 	int j;
@@ -111,6 +111,51 @@ void		ft_subsequence(int arr[], int n, int max)
 	i = -1;
 	while (++i < max)
 		printf("%d\n", arr[vet[i]]);
+} */
+
+void	ft_subsequence(int arr[], int n, int max)
+{
+	int i;
+	int j;
+	int count;
+	int	vet[n];
+	int m[max];
+
+	i = -1;
+	while (++i < n)
+		vet[i] = 0;
+	i = 0;
+	vet[i] = 1;
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			if (arr[j] < arr[i])
+			{
+				if (vet[i] < vet[j] + 1)
+					vet[i] = vet[j] + 1;
+			}
+			else if (arr[j] > arr[i] && !vet[i])
+				vet[i] = 1;
+			j++;
+		}
+		i++;
+	}
+	i = n - 1;
+	count = max;
+	while (count > 0)
+	{
+		if (vet[i] == count)
+		{
+			count--;
+			m[count] = i;
+		}
+		i--;
+	}
+	i = -1;
+	while (++i < max)
+		printf("%d\n", arr[m[i]]);
 }
 
 int		ft_lis(int arr[], int n)
@@ -125,7 +170,7 @@ int		ft_lis(int arr[], int n)
 
 int main()
 {
-    int arr[] = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+    int arr[] = { 8, 0, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15, 55, 45, 76, 16};
     int n = sizeof(arr) / sizeof(arr[0]);
 	//ft_subsequence(arr, n);
     printf("vet of lis is %d\n", ft_lis(arr, n));
